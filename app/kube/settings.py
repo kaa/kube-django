6867ec42791ba891a1596842d6d10f373d36d02c
Joakim Beijar <jbeijar@mios.fi>
2018-04-08 23:11:41 +0300

    Run on 80

--- app/kube/settings.py
+++ app/kube/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3jw0)6&g=*y!q7!!)k%oe#pn)@_kzk@y-f08*uuwf)i*&qb#3n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
